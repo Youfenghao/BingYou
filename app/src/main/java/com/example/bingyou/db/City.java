02db3a77d93b5173f2e77983188f32e746ba24f3
Mike <2036656973@qq.com>
2019-02-12 13:50:50 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/bingyou/db/City.java
@@ -0,0 +1,50 @@
+package com.example.bingyou.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 米 on 2019/2/12.
+ */
+
+public class City extends DataSupport{
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/bingyou/db/County.java
@@ -0,0 +1,50 @@
+package com.example.bingyou.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 米 on 2019/2/12.
+ */
+
+public class County extends DataSupport{
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
